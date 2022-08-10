ff5710afedc87f83a4c36908703cd0364f566925
Mila Pindianti <110595502+pindianti@users.noreply.github.com>
2022-08-10 21:15:14 +0700

    create Common Table Expression.sql

---
+++ Common Table Expression.sql
@@ -0,0 +1,2 @@
+#Common Table Expression (CTE) adalah salah satu bentuk query SQL yang digunakan untuk menyederhanakan JOIN pada SQL kedalam subqueries 
+#Serta mampu memberikan query yang bersifat hieararki
