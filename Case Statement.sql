7a3b1c3004e9499a725aef977b7f9f27c15b30d7
Mila Pindianti <110595502+pindianti@users.noreply.github.com>
2022-08-11 14:44:08 +0700

    Update Case Statement.sql

--- Case Statement.sql
+++ Case Statement.sql
@@ -1,1 +1,41 @@
-#Case statement dalam SQL digunakan untuk mengembalikan nilai pada kondisi tertentu
+#common simple case
+SELECT id_produk, stock,
+CASE stock
+  WHEN '10' THEN 'LOW'
+  WHEN '20' THEN 'MEDIUM'
+  WHEN '30' THEN 'HIGH'
+END AS deskripsi
+FROM produk;
+
+#simple case dengan opsi ELSE
+SELECT id_produk, stock, 
+CASE stock
+  WHEN '10' THEN 'LOW'
+  WHEN '20' THEN 'MEDIUM'
+  WHEN '30' THEN 'HIGH'
+  ELSE 'Belum Terdaftar' 
+END AS deskripsi 
+FROM produk;
+
+#Searched case dengan query case WHEN SQL
+SELECT id_produk, nama_produk, stock,
+CASE 
+  WHEN nama_produk = 'kelaya' THEN 'shampo herbal'
+  WHEN nama_produk = 'kitkat' THEN 'makanan ringan'
+  WHEN nama_produk = 'aqua' THEN 'minuman'
+END AS deskripsi
+FROM produk;
+
+#Searched case dengan query case WHEN SQL dan opsi ELSE
+SELECT id_produk, nama_produk, stock,
+CASE 
+  WHEN nama_produk = 'kelaya' THEN 'shampo herbal'
+  WHEN nama_produk = 'kitkat' THEN 'makanan ringan'
+  WHEN nama_produk = 'aqua' THEN 'minuman'
+  ELSE 'belum terdaftar'
+END AS deskripsi 
+FROM produk;
+
+#Nested case (case dalam IF ELSE)
+
+  
